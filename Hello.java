fe9a2486e905397ec4822d085fe750aa0a16afe4
jessecpittard <112021344+jessecpittard@users.noreply.github.com>
2022-08-24 15:49:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Jesse Pittard");
+		System.out.println("Sublime Text");
+		System.out.println("Lab 1 Introduction to the Course");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC 255-001");
+
+	}
+}
